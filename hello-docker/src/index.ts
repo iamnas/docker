66d42ea4c53a3e153b888170584be777682787b5
iamnas <iamnas.eth@gmail.com>
2025-03-02 13:49:58 +0530

    add Dockerfiles and application code for hello-docker and docker-prisma services

---
+++ hello-docker/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({
+        message:'Hello from docker!'
+    })
+})
+
+app.listen(3000,()=>{
+    console.log(`app is listening on http://localhost:${3000}`);
+    
+})
